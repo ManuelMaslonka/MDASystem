f5119d865d616407d417955f2d6e12ef0ce8c7f9
Manuel Mašlonka <manuelmaslonka@icloud.com>
2024-11-24 14:03:19 +0100

    change DomainRepository because main logic for throwing execption need to be in domain

--- account/account-domain/src/main/java/com/maslonka/mda/system/account/domain/utills/DomainRepository.java
+++ account/account-domain/src/main/java/com/maslonka/mda/system/account/domain/utills/DomainRepository.java
@@ -1,7 +1,9 @@
 package com.maslonka.mda.system.account.domain.utills;
 
+import java.util.Optional;
+
 public interface DomainRepository<T extends AggregateRoot<I>, I> {
-    T read(I id);
+    Optional<T> read(I id);
     void create(T aggregate);
     void delete(I id);
     void update(T aggregate);
